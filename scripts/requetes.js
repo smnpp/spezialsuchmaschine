87d4503046fd7e9d995ab4b16f7dc2310a36951a
nihalbgt <nihal.boutadgha@insa-lyon.fr>
2024-12-13 23:12:14 +0100

    Ajout des requetes SPARQL requete_marques et requete_details_marque

--- scripts/requetes.js
+++ scripts/requetes.js
@@ -113,3 +113,45 @@ OPTIONAL {
 }
 }`;
 }
+
+export function requete_marques() {
+    return `
+            SELECT DISTINCT ?brand ?label ?foundingDate ?introduced ?logo ?thumbnail
+            WHERE {
+              ?car rdf:type dbo:Automobile ;
+                  dbp:manufacturer ?brand .
+              ?brand dbp:locationCountry "Germany"@en .
+              OPTIONAL { ?brand rdfs:label ?label FILTER (lang(?label) = "en") }
+              OPTIONAL { ?brand dbo:foundingDate ?foundingDate }
+              OPTIONAL { ?brand dbp:introduced ?introduced }
+              OPTIONAL { ?brand dbo:foundingYear ?foundingDate }
+              OPTIONAL { ?brand dbp:logo ?logo }
+              OPTIONAL { ?brand dbo:thumbnail ?thumbnail }
+            }
+            `;
+}
+
+// Requête SPARQL pour récupérer les informations de la marque
+export function requete_details_marque(brandURI) {
+  return `
+      SELECT DISTINCT ?label ?abstract ?logo ?thumbnail ?foundingDate ?introduced ?country ?website  
+                      ?revenue ?product ?ownersLabel
+      WHERE {
+          <${brandURI}> rdfs:label ?label ;
+                       dbo:abstract ?abstract ;
+                       dbo:thumbnail ?thumbnail .
+          OPTIONAL { <${brandURI}> dbo:foundingDate ?foundingDate }
+          OPTIONAL { <${brandURI}> dbp:introduced ?introduced }
+          OPTIONAL { <${brandURI}> dbp:logo ?logo }
+          OPTIONAL { <${brandURI}> dbo:country ?country }
+          OPTIONAL { <${brandURI}> foaf:homepage ?website }
+          OPTIONAL { <${brandURI}> dbp:revenue ?revenue }
+          OPTIONAL { <${brandURI}> dbp:products ?product }
+          OPTIONAL { <${brandURI}> dbo:owners ?owners.
+                      ?owners rdfs:label ?ownersLabel }
+          FILTER (lang(?label) = "en" && lang(?abstract) = "en")
+      }
+      LIMIT 1
+  `;
+}
+
