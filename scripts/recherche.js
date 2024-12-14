5b8a1993e364f9883429490cf98adcb337f8451e
smnpp <simonperret69@gmail.com>
2024-12-14 23:26:50 +0100

    ajout des marques dans la barre de recherche

--- scripts/recherche.js
+++ scripts/recherche.js
@@ -1,6 +1,23 @@
 // Récupération des éléments de l'interface
 const searchInput = document.getElementById("search");
 
+// Fonction pour exécuter une requête SPARQL
+async function executeSparqlQuery(sparqlQuery) {
+    const endpointUrl = "https://dbpedia.org/sparql";
+    const fullUrl = `${endpointUrl}?query=${encodeURIComponent(
+        sparqlQuery
+    )}&format=json`;
+
+    try {
+        const response = await fetch(fullUrl);
+        const data = await response.json();
+        return data.results.bindings; // Retourne les résultats
+    } catch (error) {
+        console.error("Erreur lors de la requête SPARQL :", error);
+        return [];
+    }
+}
+
 // Fonction pour créer dynamiquement le conteneur des résultats
 function createResultsContainer() {
     let container = document.getElementById("search-autocomplete");
@@ -30,7 +47,7 @@ function removeResultsContainer() {
 }
 
 // Fonction pour afficher les résultats dans le conteneur
-function displayResults(results) {
+function displayResults(modelsResults, brandsResults) {
     let container = document.getElementById("search-autocomplete");
 
     // Si le conteneur n'existe pas, créez-le
@@ -44,46 +61,70 @@ function displayResults(results) {
     // Réinitialiser le conteneur
     container.innerHTML = "";
 
-    if (results.length === 0) {
-        container.innerHTML = "<div>Aucun résultat trouvé.</div>";
-        return;
-    }
+    const titleModel = document.createElement("h3");
+    titleModel.textContent = "Modèles";
+    titleModel.style.margin = "10px 0";
+    titleModel.style.color = "#333";
+    container.appendChild(titleModel);
 
-    // Ajoutez les résultats sous forme de lien
-    results.forEach(result => {
-        const suggestion = document.createElement("div");
-        suggestion.textContent = result.modelLabel.value;
-        suggestion.style.padding = "10px";
-        suggestion.style.cursor = "pointer";
-        suggestion.style.borderBottom = "1px solid #f0f0f0";
-
-        // Créer un lien vers la page du modèle
-        const modelLink = document.createElement("a");
-        modelLink.href = `modele.html?name=${encodeURIComponent(
-            result.model.value.split("/").pop()
-        )}`;
-        modelLink.textContent = result.modelLabel.value;
-        modelLink.style.textDecoration = "none";
-        modelLink.style.color = "black";
-
-        suggestion.innerHTML = "";
-        suggestion.appendChild(modelLink);
-
-        suggestion.addEventListener("click", () => {
-            window.location.href = modelLink.href;
-            container.innerHTML = ""; // Vider les résultats après sélection
-        });
-
-        suggestion.addEventListener("mouseover", () => {
-            suggestion.style.backgroundColor = "#f4f4f4";
-        });
-
-        suggestion.addEventListener("mouseout", () => {
-            suggestion.style.backgroundColor = "white";
-        });
-
-        container.appendChild(suggestion);
+    modelsResults.forEach(result => {
+        addLineResult(
+            "modele",
+            result.modelLabel.value,
+            result.model.value,
+            container
+        );
     });
+
+    const brandTitle = document.createElement("h3");
+    brandTitle.textContent = "Marques";
+    brandTitle.style.margin = "10px 0";
+    brandTitle.style.color = "#333";
+    container.appendChild(brandTitle);
+
+    brandsResults.forEach(result => {
+        addLineResult(
+            "marque",
+            result.manufacturerLabel.value,
+            result.manufacturer.value,
+            container
+        );
+    });
+}
+
+function addLineResult(type, label, name, container) {
+    const suggestion = document.createElement("div");
+    suggestion.textContent = label;
+    suggestion.style.padding = "10px";
+    suggestion.style.cursor = "pointer";
+    suggestion.style.borderBottom = "1px solid #f0f0f0";
+
+    // Créer un lien vers la page du modèle
+    const modelLink = document.createElement("a");
+    modelLink.href = `${type}.html?name=${encodeURIComponent(
+        name.split("/").pop()
+    )}`;
+    modelLink.textContent = label;
+    modelLink.style.textDecoration = "none";
+    modelLink.style.color = "black";
+
+    suggestion.innerHTML = "";
+    suggestion.appendChild(modelLink);
+
+    suggestion.addEventListener("click", () => {
+        window.location.href = modelLink.href;
+        container.innerHTML = ""; // Vider les résultats après sélection
+    });
+
+    suggestion.addEventListener("mouseover", () => {
+        suggestion.style.backgroundColor = "#f4f4f4";
+    });
+
+    suggestion.addEventListener("mouseout", () => {
+        suggestion.style.backgroundColor = "white";
+    });
+
+    container.appendChild(suggestion);
 }
 
 // Gestion de l'événement de saisie dans la barre de recherche
@@ -99,9 +140,11 @@ searchInput.addEventListener("input", async () => {
         return;
     }
 
-    const sparqlQuery = getDeutchModelSearchQuery(userInput);
-    const results = await executeSparqlQuery(sparqlQuery);
-    displayResults(results);
+    const modelsQuery = getDeutchModelSearchQuery(userInput);
+    const modelsResults = await executeSparqlQuery(modelsQuery);
+    const brandsQuery = getDeutchCarBrand(userInput);
+    const brandsResults = await executeSparqlQuery(brandsQuery);
+    displayResults(modelsResults, brandsResults);
 });
 
 // Supprimer le conteneur lorsque l'utilisateur clique en dehors
